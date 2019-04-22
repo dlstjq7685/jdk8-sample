77cef27171e03d37c6e14d8a4b848ae9fd8bf3fe
dlstjq7685 <dlstjq7685@gmail.com>
2019-04-22 21:28:00 +0900

    feat: collections description add

--- src/com/ysd/collections/basic.java
+++ src/com/ysd/collections/basic.java
@@ -4,6 +4,28 @@ import java.util.*;
 
 public class basic {
 
+    /*
+        HashSet
+            - 配列型
+            - 格納値が重複不可
+            - 並び順序は保証しない
+            - 処理速度が速い
+        TreeSet
+            - 重複値使用不可
+            - 並び順序を保証する
+        LinkedHashSet
+            - 重複値格納不可
+
+        LinkedHashMap
+            - キーと値を格納する
+            - キーは重複不可、値は重複可能
+        ArrayDeque
+            - 先頭、末尾から変数の出したり入れたり可能
+        ArrayList
+        LinkedList
+        HashMap
+        TreeMap
+     */
     private HashSet hs;
     private TreeSet ts;
     private LinkedHashSet lhs;
