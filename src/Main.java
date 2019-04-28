1d7463b51190b378d6b48961d4e5f2ea82fe636b
dlstjq7685 <dlstjq7685@gmail.com>
2019-04-29 07:17:20 +0900

    feat: lambda tutorial add

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
         basic tutorial1 = new basic();
 
-        com.ysd.threads.basic b = new com.ysd.threads.basic();
+
+        com.ysd.lambda.basic b = new com.ysd.lambda.basic();
 
 
     }
---
+++ src/com/ysd/lambda/basic.java
@@ -0,0 +1,64 @@
+package com.ysd.lambda;
+
+interface NumbericTest{
+    boolean computeTest(int n);
+}
+
+interface MyGreeting {
+    String processName(String str);
+}
+
+interface MyGeneric<T> {
+    T compute(T t);
+}
+
+
+public class basic {
+
+    public basic(){
+
+        NumbericTest isEven = (n) -> (n % 2) == 0;
+        NumbericTest isNegative = (n) -> (n < 0);
+
+        // Output: false
+        System.out.println(isEven.computeTest(5));
+
+        // Output: true
+        System.out.println(isNegative.computeTest(-5));
+
+        MyGreeting morningGreeting = (str) -> "Good Morning " + str + "!";
+        MyGreeting eveningGreeting = (str) -> "Good Evening " + str + "!";
+
+        // Output: Good Morning Luis!
+        System.out.println(morningGreeting.processName("Luis"));
+
+        // Output: Good Evening Jessica!
+        System.out.println(eveningGreeting.processName("Jessica"));
+
+        MyGeneric<String> reverse = (str) -> {
+            String result = "";
+
+            for(int i = str.length()-1; i >= 0; i--)
+                result += str.charAt(i);
+
+            return result;
+        };
+
+        // Integer version of MyGeneric
+        MyGeneric<Integer> factorial = (Integer n) -> {
+            int result = 1;
+
+            for(int i=1; i <= n; i++)
+                result = i * result;
+
+            return result;
+        };
+
+        // Output: omeD adbmaL
+        System.out.println(reverse.compute("Lambda Demo"));
+
+        // Output: 120
+        System.out.println(factorial.compute(5));
+
+    }
+}
